f1171f756961971597a09ea9ea9136096f7cd7d3
Marc_Johnson <marcjohnson518@gmail.com>
2025-11-03 13:18:32 +0000

    Fix build config

--- yieldy/apps/web/react-router.config.ts
+++ yieldy/apps/web/react-router.config.ts
@@ -3,6 +3,4 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	appDirectory: './src/app',
 	ssr: true,
-	// Disable prerender - requires DATABASE_URL at build time
-	prerender: [],
 } satisfies Config;
